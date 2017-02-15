dd68b88fe3978d8a416e0688d5279e2a331c2950
Vitaly Serov <serovvitaly@gmail.com>
2017-02-15 08:27:16 -0500

    vkgroup template

--- vkgroup/views.py
+++ vkgroup/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
-# Create your views here.
+
+def list(request):
+    content = render_to_string('vkgroup/list/index.html', {'title': 'VK groups list'})
+    return HttpResponse(status=201, content=content)
